7737f491237140f4d9ef8efef4eca13d6ab4a20d
Admin <admin@Admins-Mac.local>
2018-09-11 08:00:14 +0300

    Initial Commit

---
+++ BeePartTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeePartTime
+//
+//  Created by Admin on 2018/9/11.
+//  Copyright © 2018年 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
