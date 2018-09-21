d442112129046580e82d1da6b4c413de34ece055
Leon Cheng <nokia6102@gmail.com>
2018-09-21 22:52:44 +0800

    tabview 中間的大button 提至上層 self.view 上

--- BeePartTime/myTabBarController.swift
+++ BeePartTime/myTabBarController.swift
@@ -1,12 +1,41 @@
-//
-//  myTabBarController.swift
-//  BeePartTime
-//
-//  Created by Leon Cheng on 2018/9/21.
-//  Copyright © 2018年 Admin. All rights reserved.
-//
 
 import UIKit
+
+private var pTouchAreaEdgeInsets: UIEdgeInsets = .zero
+
+//extension UIButton {
+//
+//    var touchAreaEdgeInsets: UIEdgeInsets {
+//        get {
+//            if let value = objc_getAssociatedObject(self, &pTouchAreaEdgeInsets) as? NSValue {
+//                var edgeInsets: UIEdgeInsets = .zero
+//                value.getValue(&edgeInsets)
+//                return edgeInsets
+//            }
+//            else {
+//                return .zero
+//            }
+//        }
+//        set(newValue) {
+//            var newValueCopy = newValue
+//            let objCType = NSValue(uiEdgeInsets: .zero).objCType
+//            let value = NSValue(&newValueCopy, withObjCType: objCType)
+//            objc_setAssociatedObject(self, &pTouchAreaEdgeInsets, value, .OBJC_ASSOCIATION_RETAIN)
+//        }
+//    }
+//
+//    open override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+//        if UIEdgeInsetsEqualToEdgeInsets(self.touchAreaEdgeInsets, .zero) || !self.isEnabled || self.isHidden {
+//            return super.point(inside: point, with: event)
+//        }
+//
+//        let relativeFrame = self.bounds
+//        let hitFrame = UIEdgeInsetsInsetRect(relativeFrame, self.touchAreaEdgeInsets)
+//
+//        return hitFrame.contains(point)
+//    }
+//}
+
 
 class myTabBarController: UITabBarController {
 
@@ -16,8 +45,12 @@ class myTabBarController: UITabBarController {
         if UIImage(named: "bee") != nil {
             self.addCenterButton(withImage: #imageLiteral(resourceName: "bee"), highlightImage: #imageLiteral(resourceName: "bee"))
         }
-    }
+    
 
+    }
+    
+    
+    
     
     @objc func handleTouchTabbarCenter(sender : UIButton)
     {
@@ -25,9 +58,6 @@ class myTabBarController: UITabBarController {
         {
             let i = floor(Double(count / 2))
             self.selectedViewController = self.viewControllers?[Int(i)]
-            
-            
-            
         }
     }
     
@@ -40,29 +70,20 @@ class myTabBarController: UITabBarController {
         button.frame = CGRect(x: 0.0, y: 0.0, width: buttonImage.size.width / 2.0, height: buttonImage.size.height / 2.0)
         button.setBackgroundImage(buttonImage, for: .normal)
         button.setBackgroundImage(highlightImage, for: .highlighted)
+  
+//        button.touchAreaEdgeInsets = UIEdgeInsets(top: -10 , left: -10 , bottom: -10, right: -10)
         
-        //--button title調整
-//        button.titleEdgeInsets = UIEdgeInsetsMake(buttonImage.size.height / 2.0 + 10.0,0,0,0)
-//        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 12)
+        let vrectBound = self.view.bounds
+        let vxx = vrectBound.midX
+        let vyy = self.view.bounds.size.height
 
-//        button.setTitleColor(.gray, for: .normal)
-//        button.setTitle("即時定位", for: UIControlState.normal)
+        button.center = CGPoint(x: vxx, y: vyy - paddingBottom - 30)
         
-//        button.setTitleColor(.blue, for: .highlighted)
-//        button.setTitle("即時定位2", for: UIControlState.highlighted)
-        //--
-        let rectBoundTabbar = self.tabBar.bounds
-        let xx = rectBoundTabbar.midX
-        let yy = rectBoundTabbar.midY - paddingBottom
-        button.center = CGPoint(x: xx, y: yy)
+                button.addTarget(self, action: #selector(handleTouchTabbarCenter), for: .touchUpInside)
+                self.view.addSubview(button)
+                self.view.bringSubview(toFront: button)
         
-        
-    
-        
-        self.tabBar.addSubview(button)
-        self.tabBar.bringSubview(toFront: button)
-        
-        button.addTarget(self, action: #selector(handleTouchTabbarCenter), for: .touchUpInside)
+
         
         if let count = self.tabBar.items?.count
         {
